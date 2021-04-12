8a8355b04d3890cd9a6d58a63c93728302103fd9
hardliner66 <hardliner66@gmail.com>
2021-04-12 13:28:30 +0200

    add docs + tests

--- README.md
+++ README.md
@@ -4,11 +4,24 @@ Returns one of two parts, depending on the evaluation of an expression.
 
 [![Crates.io](https://img.shields.io/crates/v/iif)](https://crates.io/crates/iif)
 
-## Example
+## Examples
 
+Use iif to decide between two values depending on a boolean expression.
 ```rust
 use iif::iif;
 
 let active = true;
-let active_text = iif!(active, "Active", "Inactive")
+let active_text = iif!(active, "Active", "Inactive");
+
+assert_eq!(active_text, "Active");
+```
+
+Use iif_opt to create an `Option` depending on a boolean expression.
+```rust
+use iif::iif_opt;
+
+let active = true;
+let active_text = iif_opt!(active, "Active");
+
+assert_eq!(active_text, Some("Active"));
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,19 @@
 #[macro_export]
+/// Returns one of two parts, depending on the evaluation of an expression.
+/// Example:
+/// ```rust
+/// use iif::iif;
+///
+/// let active = true;
+/// let active_text = iif!(active, "Active", "Inactive");
+///
+/// assert_eq!(active_text, "Active");
+///
+/// let active = false;
+/// let active_text = iif!(active, "Active", "Inactive");
+///
+/// assert_eq!(active_text, "Inactive");
+/// ```
 macro_rules! iif {
     ( $check:expr, $true_case:expr, $false_case:expr ) => {
         if $check { $true_case } else { $false_case }
@@ -6,6 +21,21 @@ macro_rules! iif {
 }
 
 #[macro_export]
+/// Returns an option, depending on the evaluation of an expression.
+/// Example:
+/// ```rust
+/// use iif::iif_opt;
+///
+/// let active = true;
+/// let active_text = iif_opt!(active, "Active");
+///
+/// assert_eq!(active_text, Some("Active"));
+///
+/// let active = false;
+/// let active_text = iif_opt!(active, "Active");
+///
+/// assert_eq!(active_text, None);
+/// ```
 macro_rules! iif_opt {
     ( $check:expr, $true_case:expr ) => {
         if $check { Some($true_case) } else { None }
